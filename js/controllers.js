47ba42585e49d40f47ecbb427495b47edf716e0f
meredithmmyers <myers.mm89@gmail.com>
2014-02-26 10:32:22 -0500

    Properly load data into model and propagate to directive

--- js/controllers.js
+++ js/controllers.js
@@ -3,8 +3,30 @@
 /* Controllers */
 
 angular.module('modelCo.controllers', []).
-  controller('ChartCtrl', ['$scope', function($scope) {
+  controller('ChartCtrl', ['$scope', '$http', function($scope, $http) {
+    d3.csv("data/formatted-messes.csv", function(error, data) {
+      $scope.parseData(data);
+      $scope.data = data;
+      $scope.$apply();
+    });
+    $scope.parseData = function(data) {
+      _.each(data, function(d) {
+        // Dates are 100 years off, correct
+        var dateIn = new Date(d.datein);
+        var dateOut = new Date(d.dateout);
+        var correctedIn = dateIn.getFullYear() - 100;
+        var correctedOut = dateOut.getFullYear() - 100;
+        var cDateIn = dateIn.setFullYear(correctedIn);
+        var cDateOut = dateOut.setFullYear(correctedOut);
+        d.datein = new Date(cDateIn).toDateString();
+        d.dateout = new Date(cDateOut).toDateString();
+        d.mess = +parseInt(d.mess);
+        d.age = +parseInt(d.age);
+        d.heightin = +parseInt(d.heightin);
+      });
+    }
     $scope.age = function() {
+      console.log($scope.data)
       console.log("set age")
     }
     $scope.height = function() {
--- js/directives.js
+++ js/directives.js
@@ -3,232 +3,231 @@
 /* Directives */
 
 angular.module('modelCo.directives', []).
-  directive('appVersion', ['version', function(version) {
+directive('appVersion', ['version',
+  function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]).
-  directive('chart', ['d3Service', function(d3Service) {
+  }
+]).
+directive('chart', ['d3Service',
+  function(d3Service) {
     return {
-      restrict: 'A',
-      scope: {},
+      restrict: 'E',
+      scope: {
+        data: "="
+      },
       link: function(scope, element, attrs) {
-        d3Service.d3().then(function(d3) {
-            var margin = {
-              top: 20,
-              right: 20,
-              bottom: 130,
-              left: 40
-            },
-              width = 960 - margin.left - margin.right,
-              height = 600 - margin.top - margin.bottom;
+        // watch for data changes and re-render
+        scope.$watch('data', function(newVals, oldVals) {
+          if(newVals)
+            return render(newVals, 'age');
+          else
+            return;
+        }, false);
 
-            var sets = {
-              age: {x: 'name', y: 'age'},
-              mess: {x: 'name', y: 'mess'},
-              height: {x: 'name', y: 'heightin'}
+        var margin = {
+          top: 20,
+          right: 20,
+          bottom: 130,
+          left: 40
+        },
+          width = 960 - margin.left - margin.right,
+          height = 600 - margin.top - margin.bottom;
+
+        var sets = {
+          age: {
+            x: 'name',
+            y: 'age'
+          },
+          mess: {
+            x: 'name',
+            y: 'mess'
+          },
+          height: {
+            x: 'name',
+            y: 'heightin'
+          }
+        };
+
+        var formatPercent = d3.format("d");
+
+        var x = d3.scale.ordinal()
+          .rangeRoundBands([0, width], .1, 0);
+
+        var y = d3.scale.linear()
+          .rangeRound([height, 0]);
+
+        var xAxis = d3.svg.axis()
+          .scale(x)
+          .orient("bottom");
+
+        var yAxis = d3.svg.axis()
+          .scale(y)
+          .ticks(5)
+          .orient("left");
+
+        var svg = d3.select(".container").append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        function render(data, attr) {
+          console.log(data)
+          var display = sets[attr];
+          x.domain(data.map(function(d) {
+            return d[display.x];
+          }));
+          var min = d3.min(data, function(d) {
+            if (d[display.y])
+              return d[display.y];
+            else
+              return undefined;
+          });
+          var max = d3.max(data, function(d) {
+            return d[display.y];
+          });
+
+          y.domain([min - 1, max]);
+
+          svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .selectAll("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0)
+            .attr("dx", "-1.8em")
+            .style("text-anchor", "end");
+
+          svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text(attr);
+
+          svg.selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) {
+              return x(d[display.x]);
+            })
+            .attr("width", x.rangeBand())
+            .attr("y", function(d) {
+              return height;
+            })
+            .attr("height", function(d) {
+              return 0;
+            })
+            .on('click', barClick);
+
+          fadeIn(data, display, true);
+          //change(data, display, true);
+
+          clickListeners(data, display);
+
+        }
+
+        function barClick(e) {
+          console.log(e);
+        }
+
+        function clickListeners(data) {
+          d3.select("#age").on("change", function(e) {
+            fadeOut();
+            clear();
+            render(data, 'age');
+          });
+          d3.select("#mess").on("change", function() {
+            fadeOut();
+            clear();
+            render(data, 'mess');
+          });
+          d3.select("#height").on("change", function() {
+            fadeOut();
+            clear();
+            render(data, 'height');
+          });
+          // d3.select('#order').on("click", function() {
+          //   change(data, display, true);
+          // });
+        }
+
+        function fadeIn(data, display, inOrder) {
+          var transition = svg.transition().duration(750),
+            delay = function(d, i) {
+              return i * 10;
             };
 
-            var formatPercent = d3.format("d");
+          transition.selectAll(".bar")
+            .delay(delay)
+            .attr("y", function(d) {
+              return y(d[display.y]);
+            })
+            .attr("height", function(d) {
+              return height - y(d[display.y]);
+            })
+        }
 
-            var x = d3.scale.ordinal()
-              .rangeRoundBands([0, width], .1, 0);
+        function fadeOut() {
+          var transition = svg.transition().duration(750),
+            delay = function(d, i) {
+              return i * 10;
+            };
 
-            var y = d3.scale.linear()
-              .rangeRound([height, 0]);
+          transition.selectAll(".bar")
+            .delay(delay)
+            .attr("y", function(d) {
+              return height;
+            })
+            .attr("height", function(d) {
+              return 0;
+            })
+        }
 
-            var xAxis = d3.svg.axis()
-              .scale(x)
-              .orient("bottom");
+        function change(data, display, inOrder) {
 
-            var yAxis = d3.svg.axis()
-              .scale(y)
-              .ticks(5)
-              .orient("left");
+          // Copy-on-write since tweens are evaluated after a delay.
+          var x0 = x.domain(data.sort(inOrder ? function(a, b) {
+              return b[display.y] - a[display.y];
+            } : function(a, b) {
+              return d3.ascending(a[display.x], b[display.x]);
+            })
+            .map(function(d) {
+              return d[display.x];
+            }))
+            .copy();
 
-            var svg = d3.select(".container").append("svg")
-              .attr("width", width + margin.left + margin.right)
-              .attr("height", height + margin.top + margin.bottom)
-              .append("g")
-              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+          var transition = svg.transition().duration(750),
+            delay = function(d, i) {
+              return i * 10;
+            };
 
-
-            d3.csv("data/formatted-messes.csv", function(error, data) {
-
-              parseData(data);
-              render(data, 'age');
-
+          transition.selectAll(".bar")
+            .delay(delay)
+            .attr("x", function(d) {
+              return x0(d[display.x]);
             });
 
-            function render(data, attr) {
-              var display = sets[attr];
-              x.domain(data.map(function(d) {
-                return d[display.x];
-              }));
-              var min = d3.min(data, function(d) {
-                if(d[display.y])
-                  return d[display.y];
-                else
-                  return undefined;
-              });
-              var max = d3.max(data, function(d) {
-                return d[display.y];
-              });
+          transition.select(".x.axis")
+            .call(xAxis)
+            .selectAll("g")
+            .delay(delay);
+        }
 
-              y.domain([min-1, max]);
 
-              svg.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + height + ")")
-                .call(xAxis)
-                .selectAll("text")
-                .attr("transform", "rotate(-90)")
-                .attr("y", 0)
-                .attr("dx", "-1.8em")
-                .style("text-anchor", "end");
 
-              svg.append("g")
-                .attr("class", "y axis")
-                .call(yAxis)
-                .append("text")
-                .attr("transform", "rotate(-90)")
-                .attr("y", 6)
-                .attr("dy", ".71em")
-                .style("text-anchor", "end")
-                .text(attr);
-
-              svg.selectAll(".bar")
-                .data(data)
-                .enter().append("rect")
-                .attr("class", "bar")
-                .attr("x", function(d) {
-                  return x(d[display.x]);
-                })
-                .attr("width", x.rangeBand())
-                .attr("y", function(d) {
-                  return height;
-                })
-                .attr("height", function(d) {
-                  return 0;
-                })
-                .on('click', barClick);
-
-              fadeIn(data, display, true);
-              //change(data, display, true);
-
-              clickListeners(data, display);
-
-            }
-
-            function barClick(e) {
-              console.log(e);
-            }
-
-            function clickListeners(data) {
-              d3.select("#age").on("change", function(e) {
-                fadeOut();
-                clear();
-                render(data, 'age');
-              });
-              d3.select("#mess").on("change", function() {
-                fadeOut();
-                clear();
-                render(data, 'mess');
-              });
-              d3.select("#height").on("change", function() {
-                fadeOut();
-                clear();
-                render(data, 'height');
-              });
-              // d3.select('#order').on("click", function() {
-              //   change(data, display, true);
-              // });
-            }
-
-            function fadeIn(data, display, inOrder) {
-              var transition = svg.transition().duration(750),
-                delay = function(d, i) {
-                  return i * 10;
-                };
-
-              transition.selectAll(".bar")
-                .delay(delay)
-                .attr("y", function(d) {
-                  return y(d[display.y]);
-                })
-                .attr("height", function(d) {
-                  return height - y(d[display.y]);
-                })
-            }
-
-            function fadeOut() {
-              var transition = svg.transition().duration(750),
-                delay = function(d, i) {
-                  return i * 10;
-                };
-
-              transition.selectAll(".bar")
-                .delay(delay)
-                .attr("y", function(d) {
-                  return height;
-                })
-                .attr("height", function(d) {
-                  return 0;
-                })
-            }
-
-            function change(data, display, inOrder) {
-
-              // Copy-on-write since tweens are evaluated after a delay.
-              var x0 = x.domain(data.sort(inOrder ? function(a, b) {
-                  return b[display.y] - a[display.y];
-                } : function(a, b) {
-                  return d3.ascending(a[display.x], b[display.x]);
-                })
-                .map(function(d) {
-                  return d[display.x];
-                }))
-                .copy();
-
-              var transition = svg.transition().duration(750),
-                delay = function(d, i) {
-                  return i * 10;
-                };
-
-              transition.selectAll(".bar")
-                .delay(delay)
-                .attr("x", function(d) {
-                  return x0(d[display.x]);
-                });
-
-              transition.select(".x.axis")
-                .call(xAxis)
-                .selectAll("g")
-                .delay(delay);
-            }
-
-            function parseData(data) {
-              _.each(data, function(d) {
-                // Dates are 100 years off, correct
-                var dateIn = new Date(d.datein);
-                var dateOut = new Date(d.dateout);
-                var correctedIn = dateIn.getFullYear() - 100;
-                var correctedOut = dateOut.getFullYear() - 100;
-                var cDateIn = dateIn.setFullYear(correctedIn);
-                var cDateOut = dateOut.setFullYear(correctedOut);
-                d.datein = new Date(cDateIn).toDateString();
-                d.dateout = new Date(cDateOut).toDateString();
-                d.mess = +parseInt(d.mess);
-                d.age = +parseInt(d.age);
-                d.heightin = +parseInt(d.heightin);
-              });
-            }
-
-            function clear() {
-              d3.selectAll('.x.axis').remove();
-              d3.selectAll('.y.axis').remove();
-            }
-        });
+        function clear() {
+          d3.selectAll('.x.axis').remove();
+          d3.selectAll('.y.axis').remove();
+        }
       }
     }
-  }]);
+  }
+]);
