99d7d778423a7b3a7d4a0785340c3b7057a556ca
meredithmmyers <myers.mm89@gmail.com>
2014-03-04 11:43:15 -0500

    Add e2e tests

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,41 +2,116 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/getting-started.md */
 
-describe('my app', function() {
+describe('index page', function() {
+
+  beforeEach(function() {
+    browser.get('index.html');
+  });
+
+  it('should render the logo', function() {
+    expect(element(by.css('.logo')).isDisplayed())
+      .toBe(true);
+  });
+
+  it('should render the controls', function() {
+    expect(element(by.css('.controls')).isDisplayed())
+      .toBe(true);
+  });
+
+  it('should render the buttons', function() {
+    element.all(by.buttonText('Individuals')).then(function(arr) {
+        expect(arr.length).toEqual(1);
+    });
+
+    element.all(by.buttonText('Categories')).then(function(arr) {
+        expect(arr.length).toEqual(1);
+    });
+
+    element.all(by.buttonText('Sort')).then(function(arr) {
+        expect(arr.length).toEqual(1);
+    });    
+  });
+
+  it('should switch attribute radio buttons', function() {
+
+    // Categories
+
+    expect(element(by.repeater('cat in categories').row(3)).getText())
+      .toEqual('Enlistment');
+
+    expect(element(by.repeater('cat in categories').row(0)).getText())
+      .toEqual('Age');
+
+    // Transition to individuals display
+    element(by.id('individualsBtn')).click();
+
+    expect(element(by.repeater('cat in categories').row(0)).getText())
+      .toEqual('Age');
+
+    expect(element(by.repeater('cat in categories').row(4)).isPresent())
+      .toBe(false);
+
+    // Back to categories
+    element(by.id('categoriesBtn')).click();
+
+    expect(element(by.repeater('cat in categories').row(4)).isPresent())
+      .toBe(true);
+  });
+
+  it('should render the chart', function() {
+    expect(element(by.css('.chart-container')).isDisplayed())
+      .toBe(true);
+  });
+
+  it('should render the footer', function() {
+    expect(element(by.css('.footer')).isDisplayed())
+      .toBe(true);
+  });
+
+});
+
+describe('d3 graph', function() {
+
+  beforeEach(function() {
+    browser.get('index.html');
+  });
+
+  it('should have text labels', function() {
+    browser.findElements(by.css('text')).then(function(elems){
+      expect(elems.length).not.toEqual(0);
+    });
+  });
+
+  it('should have horizontal grids', function() {
+    browser.findElements(by.css('line.horizontalGrid')).then(function(elems){
+      expect(elems.length).not.toEqual(0);
+    });
+  });
+
+  it('should have bars', function() {
+    browser.findElements(by.css('.bar')).then(function(elems){
+      console.log(elems.length)
+      expect(elems.length).not.toEqual(0);
+    });
+  });
+});
+
+describe('routing', function() {
 
   browser.get('index.html');
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+  it('should automatically redirect to / when location hash/fragment is empty', function() {
+    expect(browser.getLocationAbsUrl()).toMatch("/");
   });
 
-
-  describe('view1', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view1');
-    });
-
-
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
-    });
-
+  beforeEach(function() {
+    browser.get('index.html#/about');
   });
 
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
-
+  it('should render about when user navigates to /about', function() {
+    expect(element.all(by.css('[ng-view] h4')).first().getText()).
+      toMatch(/Model Company is/);
   });
+
 });
+
