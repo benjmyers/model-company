ea0e8cf8651379a1932ddd96bd2afed5cd45ecad
meredithmmyers <myers.mm89@gmail.com>
2015-01-19 21:16:09 -0500

    Wipe out old app and replace with new generated app

---
+++ scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name modelCompanyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the modelCompanyApp
+ */
+angular.module('modelCompanyApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
